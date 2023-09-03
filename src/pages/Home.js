bf5542e779635a407c40649681dd27b955b210ea
jhsu79 <joshuanhsu@gmail.com>
2023-09-03 00:57:38 -0400

    css style changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
-import "./Home.css"
+import "./Home.css";
 
 function Home(props) {
-    return ( <div className='Home'></div>)
-  }
-  
-  export default Home;
+  return (
+    <div className="Home">
+      <p>hello there!</p>
+      <p>what story do you want to tell?</p>
+      <p>what do you want to make?</p>
+      <p>let's explore all the ways</p>
+      <p>how can we work together </p>
+    </div>
+  );
+}
+
+export default Home;
