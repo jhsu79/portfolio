a1c0e7cd5f62a2ede64675a194f81e9856df52a0
jhsu79 <joshuanhsu@gmail.com>
2023-09-02 23:02:31 -0400

    added key to map

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -15,8 +15,8 @@ function Projects(props) {
 
   // define a function that will return the JSX needed once we get the data
   const loaded = () => {
-    return projects.map((project) => (
-      <Project project={project}/>
+    return projects.map((project, index) => (
+      <Project project={project} key={`${project.name}-${index}`}/>
     ));
   };
 
