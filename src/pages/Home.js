4b18e5e632952ac66ec214607967161df30e02ca
jhsu79 <joshuanhsu@gmail.com>
2023-09-04 15:50:00 -0400

    stashing changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,11 @@ import "./Home.css";
 function Home(props) {
   return (
     <div className="Home">
-      <p>hello there!</p>
+      <p>hi there!</p>
       <p>what story do you want to tell?</p>
       <p>what do you want to make?</p>
       <p>let's explore all the ways</p>
-      <p>how we can work together </p>
+      <p> we can work together </p>
     </div>
   );
 }
